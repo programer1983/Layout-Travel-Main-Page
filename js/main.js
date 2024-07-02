22238204242cfadf91ee22c19a57596995eaac06
programer1983 <programer972@gmail.com>
2024-07-02 16:20:59 +0300

    create popular block with mobile adaptation

--- js/main.js
+++ js/main.js
@@ -14,9 +14,12 @@ function openNav(){
 /*==============================================================================*/
 
 const swiper = new Swiper('.swiper', {
+    direction: 'horizontal',
+    speed: 1500,
   
     pagination: {
       el: '.swiper-pagination',
+      clickable: true,
     },
 
     navigation: {
@@ -24,3 +27,24 @@ const swiper = new Swiper('.swiper', {
       prevEl: '.swiper-button-prev',
     },
   });
+
+  /* =====================================================================================  */
+
+  const detailsName1 = document.querySelector('.name-1')
+  const detailsName2 = document.querySelector('.name-2')
+  const detailsList1 = document.querySelector('.list-1')
+  const detailsList2 = document.querySelector('.list-2')
+
+
+  detailsName1.addEventListener("click", open)
+
+  function open(){
+    detailsList1.classList.toggle("active-menu")
+  }
+
+  detailsName2.addEventListener("click", open2)
+
+  function open2(){
+    detailsList2.classList.toggle("active-menu")
+  }
+  
