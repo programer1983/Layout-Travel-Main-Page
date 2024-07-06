8f1c6f7b17523c2976ddcf932912de245bd13202
programer1983 <programer972@gmail.com>
2024-07-06 13:45:35 +0300

    create footer with mobile adaptation

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 const nav = document.querySelector('.header__menu-list')
 const headerBurger = document.querySelector('.header__burger')
+const html = document.querySelector('html')
 
 
 
@@ -8,6 +9,7 @@ headerBurger.addEventListener('click', openNav)
 function openNav(){
     nav.classList.toggle('open-nav')
     headerBurger.classList.toggle('active')
+    html.classList.toggle('html-static')
 }
 
 
